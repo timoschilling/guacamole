a8adf8f6d7480fa863bac1f3b55284665e4b4cd8
Dirk Breuer <dirk.breuer@gmail.com>
2014-12-04 14:21:18 +0100

    AQL Spec: Fixed spec for custom for_in

--- spec/acceptance/aql_spec.rb
+++ spec/acceptance/aql_spec.rb
@@ -42,7 +42,9 @@ describe 'BasicAQLSupport' do
   end
 
   it 'should allow to provide a custom `FOR x IN y` part' do
-    pony_by_name = PoniesCollection.by_aql('FILTER p.name == @name', { name: 'Candy Mane' }, { for_in: 'FOR p IN ponies'}).first
+    pony_by_name = PoniesCollection.by_aql('FILTER p.name == @name',
+                                           { name: 'Candy Mane' },
+                                           { for_in: 'FOR p IN ponies', return_as: 'RETURN p'}).first
     expect(pony_by_name).to eq earth_pony
   end
 end
